e76e1ba09f8300dbf9e7f00475445cdd59a77d7d
Stefan Urban <stefan.urban@live.de>
2015-06-16 16:21:17 +0200

    removed triangle demo

--- main.cpp
+++ main.cpp
@@ -5,12 +5,15 @@
 #include <boost/thread.hpp>
 
 #include <OVR_CAPI.h>
+
+#define GL_GLEXT_PROTOTYPES
 #include <GLFW/glfw3.h>
 
 #include "EdvsImage.hpp"
 #include "TcpClient.hpp"
 
 
+#define DEBUG 1
 
 // Create io service
 boost::asio::io_service io_service;
@@ -38,9 +41,12 @@ void edvs_client_app(int argc, char* argv[])
 }
 
 
-static void oculus_rift_key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+static void oculus_rift_key_callback(GLFWwindow* window, int key, int, int action, int)
 {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+
+    if (key == GLFW_KEY_Q && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GL_TRUE);
 }
 
@@ -54,6 +60,9 @@ void oculus_rift_app()
 
         if (hmd)
         {
+            // Check if we are in direct mode
+            bool directHmdMode = (0 == (ovrHmdCap_ExtendDesktop & hmd->HmdCaps));
+
             // Get more details about the HMD.
             ovrSizei resolution = hmd->Resolution;
 
@@ -66,15 +75,34 @@ void oculus_rift_app()
                 int count;
                 GLFWmonitor** monitors = glfwGetMonitors(&count);
 
-                // TODO: select right monitor
-                const char* name;
+                // Select right monitor
+                std::cout << "Select output monitor:" << std::endl;
+
                 for (int i = 0; i < count; i++)
                 {
-                    name = glfwGetMonitorName(monitors[i]);
+                    std::cout << "[" << i << "] " << glfwGetMonitorName(monitors[i]) << std::endl;
+                }
+
+                // Get user input which monitor to use
+                int monitor_id = -1;
+
+#if DEBUG
+                monitor_id = 2;
+#endif
+
+                while (monitor_id == -1)
+                {
+                    std::cin >> monitor_id;
+
+                    if (monitor_id < 0 || monitor_id >= count)
+                    {
+                        std::cout << "Out of range!" << std::endl;
+                        monitor_id = -1;
+                    }
                 }
 
                 // Create window
-                GLFWwindow* window = glfwCreateWindow(1920, 1080, "test", monitors[2], nullptr);
+                GLFWwindow* window = glfwCreateWindow(1920, 1080, "test", monitors[monitor_id], nullptr);
 
                 if (window)
                 {
@@ -83,34 +111,46 @@ void oculus_rift_app()
                     glfwMakeContextCurrent(window);
                     glfwSwapInterval(1);
 
-                    ovrHmd_AttachToWindow(hmd, window, nullptr, nullptr);
+                    //ovrHmd_AttachToWindow(hmd, window, nullptr, nullptr);
+                    ovrHmd_ConfigureTracking(hmd, 128, 0);
+
+
+                    GLuint vbo = 0;
+                    glGenBuffers(1, &vbo);
+
+                    static bool sw = false;
 
                     // Stop application if window is closed
                     while (!glfwWindowShouldClose(window))
                     {
                         // Keep running
-                        float ratio;
                         int width, height;
                         glfwGetFramebufferSize(window, &width, &height);
-                        ratio = width / (float) height;
-                        glViewport(0, 0, width, height);
-                        glClear(GL_COLOR_BUFFER_BIT);
-                        glMatrixMode(GL_PROJECTION);
-                        glLoadIdentity();
-                        glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
-                        glMatrixMode(GL_MODELVIEW);
-                        glLoadIdentity();
-                        glRotatef((float) glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
-                        glBegin(GL_TRIANGLES);
-                        glColor3f(1.f, 0.f, 0.f);
-                        glVertex3f(-0.6f, -0.4f, 0.f);
-                        glColor3f(0.f, 1.f, 0.f);
-                        glVertex3f(0.6f, -0.4f, 0.f);
-                        glColor3f(0.f, 0.f, 1.f);
-                        glVertex3f(0.f, 0.6f, 0.f);
-                        glEnd();
 
-                        // Do buffer swap
+                        glViewport(0, 0, width, height);
+
+                        glClear(GL_COLOR_BUFFER_BIT);
+                        glClearColor(0.0f, 0.4f, 0.74f, 0.0f);
+
+
+
+                        glBindBuffer(GL_ARRAY_BUFFER, vbo); //Bind GL_ARRAY_BUFFER to our handle
+                        glEnableVertexAttribArray(0); //?
+                        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0); //Information about the array, 3 points for each vertex, using the float type, don't normalize, no stepping, and an offset of 0. I don't know what the first parameter does however, and how does this function know which array to deal with (does it always assume we're talking about GL_ARRAY_BUFFER?
+
+
+
+                        if (sw)
+                            glDrawArrays(GL_POINTS, 0, 1); //Draw the vertices, once again how does this know which vertices to draw? (Does it always use the ones in GL_ARRAY_BUFFER)
+                        sw = !sw;
+
+                        glDisableVertexAttribArray(0); //?
+                        glBindBuffer(GL_ARRAY_BUFFER, 0); //Unbind
+
+
+                        //ovrTrackingState tracking_state = ovrHmd_GetTrackingState(hmd, 0.);
+
+                        // Show frame
                         glfwSwapBuffers(window);
                         glfwPollEvents();
                     }
